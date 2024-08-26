10952650675b32b6183924673923b969793be36e
goldic <goldicstudent@gmail.com>
2024-08-27 00:21:52 +0400

    Create set_token and make_auth_headers methods

--- backend/src/spotify/service.py
+++ backend/src/spotify/service.py
@@ -1,14 +1,66 @@
+from datetime import datetime, timedelta
+
+from aiohttp import ClientSession
+
 from schemas import SpotifyPlaylistCreate
 
 
 class SpotifyService:
+    API_URL = 'https://api.spotify.com/v1'
+    access_token = None
+    access_token_creation = None
+
+    def __init__(self, client_id: str, client_secret: str):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.session = ClientSession()
+
     async def get_uniqueness(self, playlist: SpotifyPlaylistCreate):
         # Get playlist info > get tracks info > calculate uniqueness
         pass
 
-    async def __playlist_info(self, playlist_id: str):
+    async def __set_access_token(self) -> None:
+        """
+        Sets the access token and its expiry time.
+        """
+        response = await self.session.post(
+            'https://accounts.spotify.com/api/token',
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            data={
+                'grant_type': 'client_credentials',
+                'client_id': self.client_id,
+                'client_secret': self.client_secret,
+            }
+        )
+
+        if response.status != 200:
+            raise Exception(f'Failed to get access token. Response status code: {response.status})')
+
+        response_json = await response.json()
+
+        self.access_token = response_json['access_token']
+        self.access_token_creation = datetime.now()
+
+    async def __make_auth_headers(self) -> dict:
+        """
+        Creates the authentication headers for the Spotify API.
+        If token is expired, it will refresh it.
+        :return: Headers with authentication
+        """
+        if not self.access_token or datetime.now() - self.access_token_creation > timedelta(hours=1):
+            await self.__set_access_token()
+
+        return {
+            'Authorization': f"Bearer {self.access_token}"
+        }
+
+    async def __get(self, sub_url: str):
         pass
-    
+
+    async def __playlist_info(self, playlist_id: str):
+
     async def __track_info(self, track_id: str):
         pass
 
