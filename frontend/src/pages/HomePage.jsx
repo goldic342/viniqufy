420f090df77e584afc2409206612ec4f4206a089
goldic <goldicstudent@gmail.com>
2024-08-26 20:56:03 +0400

    Add home page basic functionality

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,8 +10,37 @@ import {
   Button,
 } from "@chakra-ui/react";
 import "@fontsource/montserrat/500.css";
+import { useState } from "react";
+import { musicServices } from "../data";
 
 const HomePage = () => {
+  const [url, setUrl] = useState("");
+  const [urlInvalid, setUrlInvalid] = useState(false);
+
+  const validateUrl = (passedUrl) => {
+    let urlValid = false;
+
+    if (!passedUrl) return urlValid;
+
+    musicServices.forEach((service) => {
+      if (passedUrl.includes(service.playlistUrl)) {
+        urlValid = [service, passedUrl.split(service.playlistUrl, 2)[1]];
+      }
+    });
+
+    return urlValid;
+  };
+
+  const handleClick = () => {
+    // Redirect to result page with get params
+
+    const playlistId = validateUrl(url);
+    if (!playlistId) {
+      setUrlInvalid(true);
+      return;
+    }
+  };
+
   return (
     <Container maxW={"4xl"} h={"100vh"}>
       <Flex justify={"center"} align={"center"} flexDirection={"column"} h={"100%"}>
@@ -24,9 +53,18 @@ const HomePage = () => {
           Paste your playlist link, and we&apos;ll tell you how unique your taste is!
         </Text>
         <InputGroup w={"60%"} size={"lg"} mt={"10px"}>
-          <Input placeholder="Link to your playlist" borderRadius={"2xl"} type={"url"} />
+          <Input
+            placeholder="Link to your playlist"
+            borderRadius={"2xl"}
+            type={"url"}
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            isInvalid={urlInvalid}
+          />
           <InputRightElement>
-            <Button size={"lg"}>Go</Button>
+            <Button size={"lg"} onClick={handleClick}>
+              Go
+            </Button>
           </InputRightElement>
         </InputGroup>
       </Flex>
