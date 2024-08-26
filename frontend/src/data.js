fd3b0bffe54a895596a6226f1d400d9bf56308ee
goldic <goldicstudent@gmail.com>
2024-08-26 20:05:57 +0400

    Create data file

---
+++ frontend/src/data.js
@@ -0,0 +1,10 @@
+// List of supported services
+export const musicServices = [
+  {
+    name: "Spotify",
+    playlistUrl: "https://open.spotify.com/playlist/",
+    dataKey: "sptfy",
+    color: "#1ED760",
+  },
+  // Soundcloud and yandex music will be supported later
+];
