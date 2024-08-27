d87b05824d2e2707980d77aa3e6e46741f1aebb2
goldic <goldicstudent@gmail.com>
2024-08-27 13:16:52 +0400

    Create artist and track schemas. Add uuid auto generation

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,4 +1,4 @@
-from uuid import UUID
+from uuid import UUID, uuid4
 
 from pydantic import BaseModel
 
@@ -6,5 +6,16 @@ from pydantic import BaseModel
 class Playlist(BaseModel):
     name: str
     owner: str
-    id: UUID
+    id: UUID = uuid4()
     tracks_count: int
+
+
+class Artist(BaseModel):
+    name: str
+    id: UUID = uuid4()
+
+
+class Track(BaseModel):
+    name: str
+    id: UUID = uuid4()
+    artists: list[Artist]
