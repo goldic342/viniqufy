6e3afa92586a208695c0647f97e0f63a24107c8a
goldic <goldicstudent@gmail.com>
2024-08-31 15:37:40 +0400

    Create global config

---
+++ backend/src/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    SPOTIFY_CLIENT_ID: str
+    SPOTIFY_CLIENT_SECRET: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
+print(settings.model_dump())
