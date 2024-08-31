2452bf572a36f6a5310e5b650a281a7af6c0252a
goldic <goldicstudent@gmail.com>
2024-09-01 01:10:10 +0400

    Add random pharase choice based on uniqueness

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -8,3 +8,43 @@ export const musicServices = [
   },
   // Soundcloud and yandex music will be supported later
 ];
+
+export const analysisPhases = [
+  {
+    max: 20,
+    phrases: [
+      "Oh, this is so mainstream!",
+      "Your playlist is like déjà vu — I've heard this before!",
+      "Well, it's kinda...",
+    ],
+    color: "#FF0000",
+  },
+  {
+    max: 40,
+    phrases: ["Well, it's could be worse", "It's like turning on the radio during rush hour"],
+    color: "#FF6347",
+  },
+  {
+    max: 60,
+    phrases: ["A mix of old and new", "You've got both classics and trends here"],
+    color: "#FFD700",
+  },
+  {
+    max: 80,
+    phrases: [
+      "A playlist for true connoisseurs!",
+      "With this playlist, you're definitely on trend",
+      "Cool, but not 100%",
+    ],
+    color: "#32CD32",
+  },
+  {
+    max: 100,
+    phrases: [
+      "Wow! A playlist like a rare butterfly!",
+      "As unique as a fingerprint!",
+      "Now you're definitely not a normal guy!",
+    ],
+    color: "#00FF7F",
+  },
+];
--- frontend/src/pages/ResultPage.jsx
+++ frontend/src/pages/ResultPage.jsx
@@ -2,6 +2,7 @@ import { useFetching } from "./../hooks/useFteching";
 import AnalysisService from "./../api/AnalysisService";
 import { Container, Flex, Heading, Spinner, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
+import { analysisPhases } from "../data";
 
 const ResultPage = () => {
   const [analysisValue, setAnalysisValue] = useState(0.0);
@@ -10,15 +11,19 @@ const ResultPage = () => {
     const analysis = await service.getSpotifyAnalysis("1mh7TCBAYggsNrvrPFQYXb");
     setAnalysisValue(analysis * 100);
   });
-  console.log(isLoading, error);
 
   useEffect(() => {
     getAnalysis();
   }, []);
 
+  const applyRandomStyleAndPhrase = (analysisValue) => {
+    const matchedPhase = analysisPhases.find((item) => analysisValue <= item.max);
+    return matchedPhase.phrases[Math.floor(Math.random() * matchedPhase.phrases.length)];
+};
+
   return (
     <Container maxW={"4xl"} h={"100vh"}>
-      <Flex justify={"center"} align={"center"} flexDirection={"column"} h={"100%"} gap={'10px'}>
+      <Flex justify={"center"} align={"center"} flexDirection={"column"} h={"100%"} gap={"10px"}>
         {error ? (
           <>
             <Heading>Hmmm... Some error occurred</Heading>
@@ -34,7 +39,7 @@ const ResultPage = () => {
         ) : (
           <>
             <Heading>{analysisValue.toFixed(1)}</Heading>
-            <Text>Some wonderful text here</Text>
+            <Text>{applyRandomStyleAndPhrase(analysisValue)}</Text>
           </>
         )}
       </Flex>
