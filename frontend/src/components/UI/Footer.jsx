121ef960bb57e043365672443fc09ecb8c213949
goldic <goldicstudent@gmail.com>
2024-09-05 16:11:24 +0400

    Update footer colors

--- frontend/src/components/UI/Footer.jsx
+++ frontend/src/components/UI/Footer.jsx
@@ -4,8 +4,8 @@ import { FaHeart } from "react-icons/fa";
 const Footer = () => {
   return (
     <Flex justify={"center"} align={"center"} bg={"gray.50"} gap={2}>
-      <Icon as={FaHeart} color={"red"} />
-      <Text>Made with Love</Text>
+      <Icon as={FaHeart} color={"red.400"} />
+      <Text color={"gray.600"}>Made with Love</Text>
     </Flex>
   );
 };
