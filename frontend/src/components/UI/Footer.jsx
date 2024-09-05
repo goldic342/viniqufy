e5d5f907b6a84533c63863ddc82bdd01aa21b945
goldic <goldicstudent@gmail.com>
2024-09-05 15:55:39 +0400

    FIX: make icons as Chakra icons

--- frontend/src/components/UI/Footer.jsx
+++ frontend/src/components/UI/Footer.jsx
@@ -1,11 +1,10 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Icon } from "@chakra-ui/react";
 import { FaHeart } from "react-icons/fa";
-import { MdSettings } from "react-icons/md";
 
 const Footer = () => {
   return (
     <Flex justify={"center"} align={"center"} bg={"gray.50"} gap={2}>
-      <FaHeart as={MdSettings} color="red" />
+      <Icon as={FaHeart} color={"red"} />
       <Text>Made with Love</Text>
     </Flex>
   );
--- frontend/src/components/UI/Header.jsx
+++ frontend/src/components/UI/Header.jsx
@@ -1,5 +1,4 @@
-import { Box, Flex, Link as ChakraLink, Spacer, Button } from "@chakra-ui/react";
-import { MdSettings } from "react-icons/md";
+import { Box, Flex, Link as ChakraLink, Spacer, Button, Icon } from "@chakra-ui/react";
 import { Link as ReactRouterLink } from "react-router-dom";
 import Logo from "./Logo";
 import { FaGithub } from "react-icons/fa";
@@ -11,7 +10,7 @@ const Header = () => {
         <Logo />
         <Spacer />
         <ChakraLink as={ReactRouterLink} to={"https://github.com/goldic342/viniqufy"}>
-          <Button colorScheme={"blue"} bg={"blue.400"} align={"inherit"} rightIcon={<FaGithub as={MdSettings} />}>
+          <Button colorScheme={"blue"} bg={"blue.400"} align={"inherit"} rightIcon={<Icon as={FaGithub}  />}>
             GitHub
           </Button>
         </ChakraLink>
