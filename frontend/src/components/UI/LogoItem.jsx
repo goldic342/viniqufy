23658d32ee8502180d46039f3a4378eaafec2a89
goldic <goldicstudent@gmail.com>
2024-09-07 11:11:22 +0400

    Create LogoItem component

---
+++ frontend/src/components/UI/LogoItem.jsx
@@ -0,0 +1,30 @@
+import { Box, Link, Image } from "@chakra-ui/react";
+import PropTypes from "prop-types";
+import { Link as ReactRouterLink } from "react-router-dom";
+
+const LogoItem = ({ src, href, alt }) => {
+  return (
+    <Link to={href} as={ReactRouterLink}>
+      <Box as="span" display="inline-block" height="40px" overflow="hidden">
+        <Image
+          src={src}
+          alt={alt}
+          height="100%"
+          opacity={0.5}
+          objectFit="contain"
+          filter="grayscale(100%)"
+          transition="filter 0.3s, opacity 0.3s"
+          _hover={{ filter: "grayscale(0%)", opacity: 0.7 }}
+        />
+      </Box>
+    </Link>
+  );
+};
+
+LogoItem.propTypes = {
+  src: PropTypes.node.isRequired,
+  href: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
+
+export default LogoItem;
