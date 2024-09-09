5589c2cf76d48f59615c761ff12d135a45f4858e
goldic <goldicstudent@gmail.com>
2024-09-09 16:45:49 +0400

    Update usePhrase

--- frontend/src/hooks/usePhrase.js
+++ frontend/src/hooks/usePhrase.js
@@ -5,20 +5,19 @@ import { useState, useEffect } from "react";
  *
  * @param {object} props
  * @prop {array} props.phrases - array of objects with max and phrases properties
- * @prop {number} props.value - value to compare with max in phrases array
- * @prop {boolean} props.trackingItem - whether to track value changes
+ * @prop {boolean} props.trackingItem - whether to track value changes and value to compare with max in phrases array
  * @returns {string} random phrase from given phrases array
  */
-export const usePhrase = ({ phrases, value, trackingItem }) => {
+export const usePhrase = ({ phrases, trackingItem }) => {
   const [phrase, setPhrase] = useState("");
 
   useEffect(() => {
     if (!trackingItem) return;
-    const matchedPhase = phrases.find((item) => value <= item.max);
+    const matchedPhase = phrases.find((item) => trackingItem <= item.max);
     setPhrase(matchedPhase.phrases[Math.floor(Math.random() * matchedPhase.phrases.length)]);
 
-    // `phrases` and `value` added because of eslintreact-hooks/exhaustive-deps
-  }, [phrases, trackingItem, value]);
+    // `phrases` added because of eslintreact-hooks/exhaustive-deps
+  }, [phrases, trackingItem]);
 
   return phrase;
 };
