c4b4eba8687905bfe30251ba088aa13f733dc2c0
goldic <goldicstudent@gmail.com>
2024-09-12 17:39:42 +0400

    Move celery config to config.py, Update celery serializer

--- backend/src/config.py
+++ backend/src/config.py
@@ -8,4 +8,23 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
 
+class CeleryConfig:
+    broker_url = 'redis://localhost:6379'
+    result_backend = 'redis://localhost:6379/0'
+
+    # Pydantic schemas support
+    task_serializer = "pickle"
+    result_serializer = "pickle"
+    accept_content = ["application/json", "application/x-python-serialize"]
+    result_accept_content = ["application/json", "application/x-python-serialize"]
+
+    imports = ('src.analysis.tasks',)
+
+    task_annotations = {
+        'src.analysis.tasks': {'rate_limit': '100/m'}
+    }
+
+    task_track_started = True
+
+
 settings = Settings()
