d2a037a844c0b6c256efdbc0c445632d3f83c260
goldic <goldicstudent@gmail.com>
2024-09-15 13:19:11 +0400

    Fix: change database uri param name

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -18,7 +18,7 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 config.set_main_option('sqlalchemy.url',
-                       str(settings.SQLALCHEMY_DATABASE_URI) + '?async_fallback=True')
+                       str(settings.ASYNC_DATABASE_URI) + '?async_fallback=True')
 
 # add your model's MetaData object here
 # for 'autogenerate' support
