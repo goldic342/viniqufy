29b7b5f7ecc283686e437d72c600947f2bca08da
goldic <goldicstudent@gmail.com>
2024-09-16 21:00:54 +0400

    Add decorator for session handling \

--- backend/src/repository.py
+++ backend/src/repository.py
@@ -1,3 +1,6 @@
+import functools
+import inspect
+
 from fastapi_async_sqlalchemy import db
 from fastapi_async_sqlalchemy.exceptions import SessionNotInitialisedError
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -5,21 +8,58 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import SessionLocal
 
 
+def with_session_management(cls):
+    class SessionManager:
+        def __init__(self):
+            self._session = None
+
+        async def __aenter__(self):
+            try:
+                self._session = db.session
+            except SessionNotInitialisedError:
+                self._session = SessionLocal()
+            return self._session
+
+        async def __aexit__(self, exc_type, exc_val, exc_tb):
+            try:
+                # Not closing session if it session from ``fastapi_async_sqlalchemy``
+                db.session
+            except SessionNotInitialisedError:
+                await self._session.close()
+                self._session = None
+
+    def wrap_method(original_method):
+        @functools.wraps(original_method)
+        async def wrapper(self, *args, **kwargs):
+            async with SessionManager() as session:
+                self._session = session
+                try:
+                    return await original_method(self, *args, **kwargs)
+                finally:
+                    self._session = None
+
+        return wrapper
+
+    for name, method in inspect.getmembers(cls, inspect.iscoroutinefunction):
+        # For methods that don't need sqlalchemy session
+        if not name.startswith('__'):
+            setattr(cls, name, wrap_method(method))
+
+    return cls
+
+
 class BaseRepository:
+    _session: AsyncSession | None
 
     def __init__(self):
         self._db = db
 
     @property
     def session(self) -> AsyncSession:
-        # Session can be taken from ``fastapi_async_sqlalchemy`` and SessionLocal.
-        # Because celery is running in different python process ``db.session`` raises SessionNotInitialisedError
-        # So catching it and returning SessionLocal, which is automatically closed
-        # during shutdown (I guess so) - no running session are opened in pgAdmin
-        try:
-            return self._db.session
-        except SessionNotInitialisedError:
-            return SessionLocal()
+        if not hasattr(self, '_session') or self._session is None:
+            raise RuntimeError(
+                "Session is not initialized. This should not happen if using the @with_session_management decorator.")
+        return self._session
 
     async def get(self, *args, **kwargs):
         raise NotImplementedError
