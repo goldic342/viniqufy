27088fb4967d44128e32c1e56db615d9467b5994
goldic <goldicstudent@gmail.com>
2024-09-16 18:31:04 +0400

    Add async_sessionmaker support for base repository

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,3 +1,8 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base
 
+from src.config import settings
+
 Base = declarative_base()
+engine = create_async_engine(str(settings.ASYNC_DATABASE_URI), echo=settings.DEBUG_MODE)
+SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
--- backend/src/repository.py
+++ backend/src/repository.py
@@ -1,5 +1,8 @@
 from fastapi_async_sqlalchemy import db
+from fastapi_async_sqlalchemy.exceptions import SessionNotInitialisedError
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database import SessionLocal
 
 
 class BaseRepository:
@@ -9,7 +12,14 @@ class BaseRepository:
 
     @property
     def session(self) -> AsyncSession:
-        return self._db.session
+        # Session can be taken from ``fastapi_async_sqlalchemy`` and SessionLocal.
+        # Because celery is running in different python process ``db.session`` raises SessionNotInitialisedError
+        # So catching it and returning SessionLocal, which is automatically closed
+        # during shutdown (I guess so) - no running session are opened in pgAdmin
+        try:
+            return self._db.session
+        except SessionNotInitialisedError:
+            return SessionLocal()
 
     async def get(self, *args, **kwargs):
         raise NotImplementedError
