79dce2d79bad35c58829918a96762738c0b80c3f
goldic <contact@goldic.xyz>
2026-04-26 21:16:30 +0400

    Adjust alembic dependencies to match import schema, update alembic file template

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,5 +1,10 @@
 from logging.config import fileConfig
 
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parents[1] / "src"))
+
 from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
